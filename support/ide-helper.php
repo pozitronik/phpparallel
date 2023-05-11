ee9f4411ead42eb70ada698035fb722d3cd39231
Pozitronik <antikillerxxl@gmail.com>
2023-05-11 18:48:37 +0400

    ide-helper

---
+++ support/ide-helper.php
@@ -0,0 +1,17 @@
+<?php /** @noinspection ALL */
+declare(strict_types = 1);
+
+namespace {
+	exit("This file should not be included, it is a only IDE helper");
+}
+
+namespace parallel {
+	/**
+	 * @method __construct(void|string $bootstrap = void)
+	 * @method run(\Closure $task, void|array $argv = void):?Future
+	 * @method close():void
+	 * @method kill():void
+	 */
+	final class Runtime {
+	}
+}
