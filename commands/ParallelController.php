d380dcc63c94a0c0736e7e529988935e3f97c9f1
Pozitronik <antikillerxxl@gmail.com>
2023-05-12 20:26:50 +0400

    examples simplified

--- commands/ParallelController.php
+++ commands/ParallelController.php
@@ -4,7 +4,8 @@ declare(strict_types = 1);
 namespace app\commands;
 
 use app\models\Tasks;
-use parallel\Channel;
+use Yii;
+use yii\console\Application;
 use yii\console\Controller;
 use parallel\Runtime;
 use yii\helpers\Console;
@@ -36,15 +37,13 @@ class ParallelController extends Controller {
 	}
 
 	/**
-	 * This example allows to run some parallel tasks simultaneously
+	 * This example allows to run some parallel tasks simultaneously. It also shows that tasks execution order is not deterministic.
 	 * @param int $threadsCnt
 	 * @param null|int $pause Pass null to random wait time for every task
 	 * @return void
 	 */
 	public function actionSampleTwo(int $threadsCnt = 10, ?int $pause = null):void {
-		$ch = new Channel();
-
-		$task = static function(Channel $ch, int $threadNumber, ?int $pause):void {
+		$task = static function(int $threadNumber, ?int $pause):void {
 			//Console::output("[enter: $threadNumber]");// ?!
 			echo("[enter: $threadNumber]\n");
 			sleep($pause??random_int(5, 20));
@@ -52,48 +51,39 @@ class ParallelController extends Controller {
 		};
 		/** @var Runtime[] $runtimeList */
 		$runtimeList = [];
+
 		for ($i = 0; $i < $threadsCnt; $i++) {
 			$runtimeList[] = new Runtime();
 		}
 
 		foreach ($runtimeList as $i => $runtime) {
 			echo("[run: $i]\n");
-			$runtime->run($task, [$ch, $i, $pause]);
+			$runtime->run($task, [$i, $pause]);
 		}
-
-		for ($i = 0; $i < $threadsCnt * $pause; $i++) {
-			Console::output($ch->recv());
-		}
-		$ch->close();
 	}
 
 	/**
-	 * This example shows how to pass bootstrap script to parallel tasks
+	 * This example shows how to pass bootstrap script to parallel tasks and how to use framework inside tasks.
 	 * @param int $threadsCnt
 	 * @param null|int $pause Pass null to random wait time for every task
 	 * @return void
 	 */
-	public function actionSampleTree(int $threadsCnt = 10, ?int $pause = 5):void {
-		$ch = new Channel();
-
-		$task = static function(Channel $ch, int $threadNumber, ?int $pause):void {
+	public function actionSampleTree(int $threadsCnt = 10, ?int $pause = null):void {
+		$config = Yii::getAlias('@app/config/console.php');
+		$task = static function(int $threadNumber, ?int $pause) use ($config):void {
+			new Application(require $config);
 			Console::output("[enter: $threadNumber]");
-			Console::output(Tasks::waiter($pause??random_int(5, 20), "[enter: $threadNumber]"));
+			Console::output(Tasks::waiter($pause??random_int(5, 20), "[exit: $threadNumber]"));
 		};
 		/** @var Runtime[] $runtimeList */
 		$runtimeList = [];
 		for ($i = 0; $i < $threadsCnt; $i++) {
-			$runtimeList[] = new Runtime(__DIR__.'/../vendor/autoload.php');
+			$runtimeList[] = new Runtime(Yii::getAlias('@app/bootstrap_console.php'));
 		}
 
 		foreach ($runtimeList as $i => $runtime) {
 			echo("[run: $i]\n");
-			$runtime->run($task, [$ch, $i, $pause]);
+			$runtime->run($task, [$i, $pause]);
 		}
-
-		for ($i = 0; $i < $threadsCnt * $pause; $i++) {
-			Console::output($ch->recv());
-		}
-		$ch->close();
 	}
 }
