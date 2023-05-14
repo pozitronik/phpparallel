a638a8826f56a5cfc75ce96ba18364c15387c2f6
Pozitronik <antikillerxxl@gmail.com>
2023-05-14 18:47:37 +0400

    Buffered channels example done

--- commands/ChannelController.php
+++ commands/ChannelController.php
@@ -16,7 +16,7 @@ use yii\helpers\Console;
 class ChannelController extends Controller {
 
 	/**
-	 * This example shows how to use unbuferred channels to set blocking communications between tasks
+	 * This example shows how to use unbuffered channels to set blocking communications between tasks
 	 * @param int $tasksCount
 	 * @return void
 	 */
@@ -25,7 +25,7 @@ class ChannelController extends Controller {
 		Console::clearScreen();
 		/** @var Runtime[] $runtimeList */
 		$runtimeList = [];
-		/* Create a Channel instance to communicate between task */
+		/* Create a unbuffered Channel instance to communicate between task */
 		$dataChannel = new Channel();
 
 		/* Create set of senders tasks */
@@ -44,12 +44,15 @@ class ChannelController extends Controller {
 			}, [$dataChannel]);
 		}
 
-		/*Create the reader (receiver) task. */
+		/* Create the reader (receiver) task */
 		$reader = new Runtime(Yii::getAlias('@app/bootstrap_console.php'));
 		$reader->run(static function(Channel $channel) use ($config):void {
 			new Application($config);
 			$taskData = [];
+			$iterations = 0;
 			while (true) {
+				Console::moveCursorTo(1, 1);
+				Console::output(sprintf("updates received: %s", $iterations));
 				[$name, $value] = $channel->recv();
 				$taskData[$name] = $value;// Console::renderColoredString(Console::ansiFormatCode([random_int(Console::FG_RED, Console::FG_GREY)]).$value."%n");
 
@@ -57,8 +60,54 @@ class ChannelController extends Controller {
 					Console::moveCursorTo(1, $name + 1);
 					Console::output(sprintf("%02s: %010s", $name, $value));
 				}
+				$iterations++;
 			}
 		}, [$dataChannel]);
 	}
 
+	/**
+	 * This example shows difference between buffered and unbuffered channels
+	 * @param int $tasksCount
+	 * @param bool $buffered
+	 * @return void
+	 */
+	public function actionExampleTwo(int $tasksCount = 3, bool $buffered = true):void {
+		$config = require Yii::getAlias('@app/config/console.php');
+		Console::clearScreen();
+		/** @var Runtime[] $runtimeList */
+		$runtimeList = [];
+		/* Create a buffered Channel instance to communicate between task */
+		$dataChannel = $buffered
+			?new Channel(Channel::Infinite)
+			:new Channel();
+
+		/* Create set of receivers tasks */
+		for ($i = 0; $i < $tasksCount; $i++) {
+			$runtimeList[] = new Runtime(Yii::getAlias('@app/bootstrap_console.php'));
+		}
+		foreach ($runtimeList as $i => $runtime) {
+			$runtime->run(static function(Channel $channel) use ($i, $config):void {
+				new Application($config);
+				while (true) {
+					$data = $channel->recv();
+					Console::moveCursorTo(1, $i + 2);
+					Console::output(sprintf("%02s: %s", $i, $data));
+					sleep(2);
+				}
+			}, [$dataChannel]);
+		}
+
+		/* Create the sender task */
+		$reader = new Runtime(Yii::getAlias('@app/bootstrap_console.php'));
+		$reader->run(static function(Channel $channel) use ($config):void {
+			new Application($config);
+			$iterations = 0;
+			while (true) {
+				Console::moveCursorTo(1, 1);
+				Console::output(sprintf("Messages written: %s", $iterations));
+				$channel->send(strtr(base64_encode(random_bytes(32)), '+/', '-_'));
+				$iterations++;
+			}
+		}, [$dataChannel]);
+	}
 }
