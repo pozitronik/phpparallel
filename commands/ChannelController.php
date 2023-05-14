be6d9f802e80ef69b640b066c99c805002204b8f
Pozitronik <antikillerxxl@gmail.com>
2023-05-14 18:13:01 +0400

    Example done

--- commands/ChannelController.php
+++ commands/ChannelController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 namespace app\commands;
 
 use parallel\Channel;
-use parallel\Future;
 use parallel\Runtime;
 use Yii;
 use yii\console\Application;
@@ -17,35 +16,32 @@ use yii\helpers\Console;
 class ChannelController extends Controller {
 
 	/**
-	 * Идея: создаём n потоков, что-то считающих, и отправляющих данные в канал + поток, который получает данные из каналов,
-	 * и выводящий данные на экран + поток, читающий команды, и управляющий потоками.
+	 * This example shows how to use unbuferred channels to set blocking communications between tasks
 	 * @param int $tasksCount
 	 * @return void
 	 */
-	public function actionOne(int $tasksCount = 3):void {
+	public function actionExampleOne(int $tasksCount = 3):void {
 		$config = require Yii::getAlias('@app/config/console.php');
 		Console::clearScreen();
 		/** @var Runtime[] $runtimeList */
 		$runtimeList = [];
-		/** @var Future[] $futuresList */
-		$futuresList = [];
 		/* Create a Channel instance to communicate between task */
-		$channel = new Channel();
+		$dataChannel = new Channel();
 
 		/* Create set of senders tasks */
 		for ($i = 0; $i < $tasksCount; $i++) {
 			$runtimeList[] = new Runtime();
 		}
 		foreach ($runtimeList as $i => $runtime) {
-			$futuresList[] = $runtime->run(static function(Channel $channel) use ($i, $tasksCount):void {
-				$rnd = strtr(base64_encode(random_bytes(32)), '+/', '-_');
+			$runtime->run(static function(Channel $channel) use ($i, $tasksCount):void {
+				$rnd = 0;
 				$pause = random_int(1, $tasksCount);//Add a random pause
 				while (true) {
-					$rnd = sha1($rnd);
+					$rnd++;
 					$channel->send([$i, $rnd]);
 					sleep($pause);
 				}
-			}, [$channel]);
+			}, [$dataChannel]);
 		}
 
 		/*Create the reader (receiver) task. */
@@ -55,15 +51,14 @@ class ChannelController extends Controller {
 			$taskData = [];
 			while (true) {
 				[$name, $value] = $channel->recv();
-				$taskData[$name] = Console::renderColoredString(Console::ansiFormatCode([random_int(Console::FG_RED, Console::FG_GREY)]).$value."%n");
+				$taskData[$name] = $value;// Console::renderColoredString(Console::ansiFormatCode([random_int(Console::FG_RED, Console::FG_GREY)]).$value."%n");
 
 				foreach ($taskData as $name => $value) {
 					Console::moveCursorTo(1, $name + 1);
-					Console::output(sprintf("%02s: %s", $name, $value));
+					Console::output(sprintf("%02s: %010s", $name, $value));
 				}
 			}
-		}, [$channel]);
-
+		}, [$dataChannel]);
 	}
 
 }
